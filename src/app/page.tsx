891a72a1827f6129eae2b87fd9e180733b7f37fc
bong <jungcome7@gmail.com>
2025-04-01 17:21:42 +0900

    feat: shadcn 설치

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { Input } from '@/components/ui/input';
+
 export default function Page() {
   return (
-    <h1 className="bg-yellow-500 text-xl font-bold underline">Hello world!</h1>
+    <div>
+      <h1 className="bg-yellow-500 text-xl font-bold underline">
+        Hello world!
+      </h1>
+      <Input placeholder="Hello" />
+    </div>
   );
 }
