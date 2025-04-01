ec9b101fb7ec42b75401f86d61027a215014d2ec
bong <jungcome7@gmail.com>
2025-04-01 13:13:05 +0900

    feat: yarn, typescript, eslint, prettier 셋업

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'react/display-name': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-key': 'warn',
+
+      semi: ['error', 'always'],
+      'no-trailing-spaces': ['error', { skipBlankLines: false }],
+      'no-multiple-empty-lines': ['error', { max: 1 }],
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+];
