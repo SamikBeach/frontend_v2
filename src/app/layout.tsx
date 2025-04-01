245f1854d1ca4f62e5e7f9746e3d6991cffff3e4
bong <jungcome7@gmail.com>
2025-04-01 16:43:38 +0900

    feat: tailwindcss 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import '@/styles/globals.css';
 import { ReactNode } from 'react';
 
 export const metadata = {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 export default function Page() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <h1 className="bg-yellow-500 text-xl font-bold underline">Hello world!</h1>
   );
 }
