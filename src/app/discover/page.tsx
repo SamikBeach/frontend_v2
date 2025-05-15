ddc82cde4d00033e93fedd319dd8a51d4bffe2b8
bong <jungcome7@gmail.com>
2025-05-15 22:50:36 +0900

    feat: 데스크톱 발견하기 페이지 로딩 시 레이아웃 안정성 개선

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -69,8 +69,10 @@ const noScrollbarStyles = `
 // 책 컨텐츠 로딩 스켈레톤
 function BooksLoading() {
   return (
-    <div className="flex h-[calc(100vh-250px)] w-full items-center justify-center">
-      <LoadingSpinner />
+    <div className="min-h-[calc(100vh-250px)] w-full">
+      <div className="flex h-full w-full items-center justify-center py-20">
+        <LoadingSpinner />
+      </div>
     </div>
   );
 }
