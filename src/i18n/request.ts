2c48c7db4fb16704c05bf4581c8a782eddba807b
bong <jungcome7@gmail.com>
2025-05-18 12:50:38 +0900

    feat: next-intl 적용

---
+++ src/i18n/request.ts
@@ -0,0 +1,13 @@
+import { getRequestConfig } from 'next-intl/server';
+import { headers } from 'next/headers';
+
+export default getRequestConfig(async () => {
+  const header = await headers();
+  const locale =
+    header.get('accept-language')?.split(',')[0].split('-')[0] ?? 'ko';
+
+  return {
+    locale,
+    messages: (await import(`../../messages/${locale}.json`)).default,
+  };
+});
